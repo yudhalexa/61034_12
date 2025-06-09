1e89293f4676b68ed85ec2425e50506d51c95f09
yudhalexa <101029025+yudhalexa@users.noreply.github.com>
2025-06-09 23:24:43 +0700

    Step 9 - Update WorkManagerBluromaticRepository

--- app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
+++ app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
@@ -24,8 +24,13 @@ import com.example.bluromatic.KEY_BLUR_LEVEL
 import com.example.bluromatic.KEY_IMAGE_URI
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import androidx.work.WorkManager
+import com.example.bluromatic.workers.BlurWorker
+import androidx.work.OneTimeWorkRequestBuilder
 
 class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
+
+    private val workManager = WorkManager.getInstance(context)
 
     override val outputWorkInfo: Flow<WorkInfo?> = MutableStateFlow(null)
 
@@ -33,8 +38,13 @@ class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
      * Create the WorkRequests to apply the blur and save the resulting image
      * @param blurLevel The amount to blur the image
      */
-    override fun applyBlur(blurLevel: Int) {}
+    override fun applyBlur(blurLevel: Int) {
+        // Create WorkRequest to blur the image
+        val blurBuilder = OneTimeWorkRequestBuilder<BlurWorker>()
 
+        // Start the work
+        workManager.enqueue(blurBuilder.build())
+    }
     /**
      * Cancel any ongoing WorkRequests
      * */
